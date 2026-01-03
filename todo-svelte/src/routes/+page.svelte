087bb639aee68ece114ff3804ddfa7c632e95905
MoritzSemmelmann <moritz.semmelmann@st.oth-regensburg.de>
2026-01-03 11:54:19 +0100

    Inital Commit

---
+++ todo-svelte/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<div class="container">
+  <div class="box has-text-centered">
+    <div class="title"> Titel</div>
+    <div class="subtitle">Subtitel</div>
+  </div>
+</div>
