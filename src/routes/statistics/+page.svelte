73326d384f7bc209d2a4468605e8d73eb7419780
MoritzSemmelmann <moritz.semmelmann@st.oth-regensburg.de>
2026-01-15 20:05:00 +0100

    Feat: added navigation to navbar

---
+++ src/routes/statistics/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import NavBar from '$lib/components/nav-bar.svelte';
+	import Footer from '$lib/components/footer.svelte';
+	import Chart from "svelte-frappe-charts";
+	import { onMount } from 'svelte';
+
+	
+</script>
+
+<div style="min-height: 100vh; display: flex; flex-direction: column;">
+	<NavBar />
+
+	<section class="section" style="flex: 1;">statistics
+	</section>
+
+	<Footer />
+</div>
+
